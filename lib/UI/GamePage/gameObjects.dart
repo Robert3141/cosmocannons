eaa34069644e79b284d04a72cbc9b912fd785907
Robert3141 <robert.aries1@gmail.com>
2021-01-06 10:27:28 +0000

    Objects go brrrr

--- lib/UI/GamePage/gameObjects.dart
+++ lib/UI/GamePage/gameObjects.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'dart:ui';
+import 'package:cosmocannons/UI/GamePage/gamePaint.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cosmocannons/globals.dart' as globals;
@@ -46,48 +47,85 @@ class Player extends GameObject {
 }
 
 class Projectile extends GameObject {
+  bool updated = false;
   Offset _u;
   Offset _a;
   Offset _startPos;
   double _timeSec;
+  int _player;
 
   Offset get aU => _u;
   Offset get aA => _a;
   Offset get aS => this.aPos;
   double get time => _timeSec;
   Offset get aStart => _startPos;
+  int get playerInt => _player;
+  Player get playerObj => globals.players[player];
 
-  Projectile(Offset u, Offset a, Offset s) {
+  Projectile(Offset u, Offset a, Offset s, int player) {
     _u = u;
     _a = a;
     _startPos = s;
     _timeSec = 0;
+    _player = player;
 
     _animateProjectile();
   }
   Projectile.radians(double intensity, double angleRadians, int player) {
-    _projectileRadians(intensity, angleRadians, player);
+    _player = player;
+    _projectileRadians(intensity, angleRadians);
   }
   Projectile.degrees(double intensity, double angleDegrees, int player) {
-    _projectileRadians(
-        intensity, angleDegrees * globals.degreesToRadians, player);
+    _player = player;
+    _projectileRadians(intensity, angleDegrees * globals.degreesToRadians);
   }
 
-  void _projectileRadians(double intensity, double angleRadians, int player) {
+  void _projectileRadians(double intensity, double angleRadians) {
     _u = Offset(intensity * -cos(angleRadians), intensity * sin(angleRadians));
     _a = Offset(globals.Ax, globals.Ay);
-    aPos = globals.players[player].aPos;
+    aPos = playerObj.aPos;
 
     _animateProjectile();
   }
 
   Future<List<double>> _animateProjectile() {
-    Ticker timer;
+    Timer timer;
     Duration length = Duration(milliseconds: globals.frameLengthMs);
 
     //run timer
-    timer = Timer.periodic(length,renderCallback(Timer timer));
+    timer = Timer.periodic(length, (timer) => _renderCallback(timer));
   }
 
-  void renderCallback(Timer timer) {}
+  void _renderCallback(Timer timer) {
+    //set time
+    bool hitPlayer;
+    double terrainHeight;
+    int tick = timer.tick;
+    _timeSec = (globals.frameLengthMs * tick * globals.animationSpeed) / 1000;
+
+    //set new locations
+    updated = true;
+    // s = ut + 0.5att
+    aX = aX + (_u.dx * _timeSec + 0.5 * aX * _timeSec * _timeSec) * globals.xSF;
+    aY = aY + (_u.dy * _timeSec + 0.5 * aY * _timeSec * _timeSec) * globals.ySF;
+
+    // hit terrain?
+    terrainHeight = GlobalPainter(player, playerTeams, playerShootSetup).calcNearestHeight(globals.currentMap, aX)
+
+    //hit player?
+    hitPlayer = false;
+    for (int p = 0; p < amountOfPlayers; p++) {
+      if (checkInRadius(aPos, playerObj.aPos, globals.blastRadius)) {
+
+      }
+    }
+  }
+
+  bool checkInRadius(
+      Offset item, Offset hitbox, double hitboxRadius) {
+    return item.dx > hitbox.dx - hitboxRadius &&
+        item.dx < hitbox.dx + hitboxRadius &&
+        item.dy > hitbox.dy - hitboxRadius &&
+        item.dy < hitbox.dy + hitboxRadius;
+  }
 }
